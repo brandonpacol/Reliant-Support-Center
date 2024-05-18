0d97aba2e0f28357d039a45eeddb1d6984921426
Brandon Pacol <pacol.brandon@gmail.com>
2024-05-17 21:00:17 -0700

    Refining the Tickets page

--- frontend/src/pages/TicketsPage.tsx
+++ frontend/src/pages/TicketsPage.tsx
@@ -42,40 +42,43 @@ function TicketsPage() {
           <h2>Tickets</h2>
 
           <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <select id="status" name="status" style={{marginRight: "1em"}}>
+            <select id="status" name="status" style={{padding: "0.5em", marginRight: "1em", borderRadius: "0.5em"}}>
               <option value="">Status</option>
               <option value="open">Open</option>
               <option value="in progress">In Progress</option>
               <option value="closed">Closed</option>
             </select>
 
-            <select id="priority" name="priority" style={{marginRight: "1em"}}>
+            <select id="priority" name="priority" style={{padding: "0.5em", marginRight: "1em", borderRadius: "0.5em"}}>
               <option value="">Priority</option>
               <option value="1">High</option>
               <option value="2">Medium</option>
               <option value="3">Low</option>
             </select>
 
-            <button onClick={handleNewTicket}>+ New Ticket</button>
+            <button style={{padding: "0.5em", font: "1em", borderRadius: "0.5em"}} onClick={handleNewTicket}>+ New Ticket</button>
           </div>
         </div>
 
-        <table style={{ width: "100%" }}>
-          <thead>
-            <tr>
-              <th>Subject</th>
-              <th>Status</th>
-              <th>Priority</th>
-              <th>Created</th>
-              <th>Updated</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tickets.map((ticket) => {
-              return <TableRow ticket={ticket} />
-            })}
-          </tbody>
-        </table>
+        <div style={{overflow: "hidden", width: "100%", borderRadius: "0.5em", display: "block", backgroundColor: "#00000005", borderColor: "#ddd", borderWidth: "0.1em", borderStyle: "solid" }}>
+          <table style={{ width: "100%" }}>
+            <thead style={{ backgroundColor: "#5D7790", color: "white" }}>
+              <tr style={{ fontSize: "1.1em" }}>
+                <th>Subject</th>
+                <th style={{ textAlign: "center" }}>Status</th>
+                <th style={{ textAlign: "center" }}>Priority</th>
+                <th style={{ textAlign: "center" }}>Created</th>
+                <th style={{ textAlign: "center" }}>Updated</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tickets.map((ticket) => {
+                return <TableRow ticket={ticket} />
+              })}
+            </tbody>
+          </table>
+        </div>
+        
       </div>
     </>
   );
@@ -89,6 +92,8 @@ interface TableRowProps {
 
 function TableRow({ticket}: TableRowProps) {
 
+  const navigate = useNavigate();
+
   const createdTime = new Date(ticket.createdTime);
   const updatedTime = new Date(ticket.updatedTime);
   const options: Intl.DateTimeFormatOptions = {
@@ -99,13 +104,17 @@ function TableRow({ticket}: TableRowProps) {
 
   const priority = getPriorityName(ticket.priority) || "N/A";
 
+  function handleRowClick() {
+    if (ticket.ticketID) navigate(`/tickets/${ticket.ticketID}`);
+  }
+
   return (
-    <tr>
+    <tr onClick={handleRowClick}>
       <td>{ticket.title}</td>
-      <td>{ticket.status}</td>
-      <td>{priority}</td>
-      <td>{createdTime.toLocaleString('en-us', options)}</td>
-      <td>{updatedTime.toLocaleString('en-us', options)}</td>
+      <td><div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><span style={{backgroundColor: "#BAC550", padding: "0.5em", borderRadius: "0.5em", width: "6em", textAlign: "center" }}>{ticket.status}</span></div></td>
+      <td><div style={{display: "flex", justifyContent: "center", alignItems: "center"}}><span style={{backgroundColor: "red", padding: "0.5em", borderRadius: "0.5em", color: "white", width: "4em", textAlign: "center"}}>{priority}</span></div></td>
+      <td style={{textAlign: "center"}}>{createdTime.toLocaleString('en-us', options)}</td>
+      <td style={{textAlign: "center"}}>{updatedTime.toLocaleString('en-us', options)}</td>
     </tr>
   )
 }
