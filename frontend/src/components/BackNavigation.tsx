80e72f8587055fb2d8a5870675ee92f38a89903e
Brandon Pacol <pacol.brandon@gmail.com>
2024-05-19 20:34:27 -0700

    Ran into an issue where pressing the back button would show data from the tickets page occasionally. Seemed to fix this by modifying the a tag in the BackNavigation component to use the navigate function instead of an href link. However, it's still an issue that the user can press back to see previous pages. At least no data shows and the user cannot add or manipulate data.

--- frontend/src/components/BackNavigation.tsx
+++ frontend/src/components/BackNavigation.tsx
@@ -1,11 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import "./BackNavigation.css";
 
 function BackNavigation() {
+
+  const navigate = useNavigate();
+
+  function handleBackPress(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+    navigate("/tickets");
+  }
+
   return (
     <div id="back-navigation">
-      <a href="/tickets"><FontAwesomeIcon id="back-chevron" icon={faChevronLeft} />{"All Tickets"}</a>
+      <a onClick={handleBackPress}><FontAwesomeIcon id="back-chevron" icon={faChevronLeft} />{"All Tickets"}</a>
     </div>
   )
 }
